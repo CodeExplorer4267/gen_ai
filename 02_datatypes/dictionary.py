4c3315139a161d3a6fc386c44f7b06bc4a8d9be2
CodeExplorer4267 <rupambhadra478@gmail.com>
2026-04-13 09:05:56 +0530

    commit

---
+++ 02_datatypes/dictionary.py
@@ -0,0 +1,14 @@
+# d=dict(one="1",two=2,three=3)
+# print(d) #{'one': 1, 'two': 2, 'three': 3}
+# #access by key 
+# print(d["one"]) 
+
+d={}
+d["hello"]=1
+d["bello"]=2
+d["cjklido"]=3
+print(d) #{'hello': 1, 'bello': 2, 'cjklido': 3}
+
+#how to delete a key value pair from dictionary
+del d["hello"]
+print(d)
---
+++ 02_datatypes/set_frozenset.py
@@ -0,0 +1,23 @@
+# a={1,2,3,4}
+# b={3,4,5,6}
+
+# # print(a.union(b))
+# # print(a | b)
+# # print(a.intersection(b))
+# # print(a & b)
+# # print(a.difference(b))
+# # print(a-b)
+
+# #membership test
+# print(f"Is 2 present in a ? {2 in a}")
+# print(f"Is 5 present in a ? {5 in a}")
+
+# a={1,2,3,4}
+# a.add(5)
+# print(a)
+
+# #frozenset 
+# fs=frozenset([1,2,3])
+# # fs.add(4) #AttributeError: 'frozenset' object has no attribute 'add'
+# print(fs)
+
