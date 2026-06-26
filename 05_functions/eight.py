a022988806e8bd8ae85ed977eacbb60f0c7e70e1
CodeExplorer4267 <rupambhadra478@gmail.com>
2026-06-26 21:31:55 +0530

    commit

---
+++ 05_functions/eight.py
@@ -0,0 +1,15 @@
+# import seven
+
+# seven.func1()
+# seven.func2()
+
+#if we want to use particular functions 
+# from seven import func1
+# from seven import func1 as Fun
+
+# Fun()
+
+# ---python libraries
+from datetime import datetime
+from requests import request
+
---
+++ 05_functions/seven.py
@@ -0,0 +1,15 @@
+# def chai_flavor():
+#     """Return the flavor of chai""" #this line needs to be the first line of the function 
+#     chai="ginger"
+#     return chai
+
+# print(chai_flavor.__doc__) #documentation of the function 
+# print(chai_flavor.__name__) #for get the name of the function 
+
+def func1():
+    print("This is function1")
+def func2():
+    print("This is function2")
+def func3():
+    print("This is function3")
+
